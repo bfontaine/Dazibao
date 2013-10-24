925263b225aab26ea0d6853e96f5dd0af96dc3f3
Baptiste Fontaine <batifon@yahoo.fr>
2013-10-24 23:35:47 +0200

    TLVs codes added.

---
+++ src/tlvs.h
@@ -0,0 +1,12 @@
+#ifndef _TLVS_H
+#define _TLVS_H 1
+
+#define TLV_PAD1     0
+#define TLV_PADN     1
+#define TLV_TEXT     2
+#define TLV_PNG      3
+#define TLV_JPEG     4
+#define TLV_COMPOUND 5
+#define TLV_DATED    6
+
+#endif
