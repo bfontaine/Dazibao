d440a5484b73865307d3bbdfb1a6cca51464e48a
Julien Sagot <ju.sagot@gmail.com>
2013-10-24 22:23:57 +0200

    fixed missing named of some arguments

--- src/dazibao.h
+++ src/dazibao.h
@@ -1,7 +1,26 @@
+#ifndef DAZIBAO_H
+#define DAZIBAO_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/file.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+#define PANIC(str) {					\
+		perror((str));				\
+		exit(EXIT_FAILURE);			\
+	}
+
+
 typedef char value_t;
 
 struct tlv {
-	unsigned byte type;
+	unsigned char type;
 	unsigned int length:24;
 	value_t *value;
 };
@@ -10,8 +29,12 @@ struct dazibao {
 	int fd;
 };
 
-/*  */
-int open_dazibao(struct dazibao* d, char* path, int flag);
+/*
+  open $(path) file with $(flag) flag
+  apply flock according to $(flag)
+  fill $(d)
+ */
+int open_dazibao(struct dazibao* d, char* path, int flags);
 
 /*  */
 int close_dazibao(struct dazibao* d);
@@ -20,16 +43,17 @@ int close_dazibao(struct dazibao* d);
 int read_tlv(struct dazibao* d, struct tlv* buf, int offset);
 
 /*  */
-int next_tlv(struct dazibao* d, struct tlv*);
+int next_tlv(struct dazibao* d, struct tlv* buf);
 
 /*  */
-int tlv_at(struct dazibao* d, struct tlv*, int offset);
+int tlv_at(struct dazibao* d, struct tlv* buf, int offset);
 
 /*  */
-int add_tlv(struct dazibao* d, struct tlv*);
+int add_tlv(struct dazibao* d, struct tlv* buf);
 
 /*  */
 int rm_tlv(struct dazibao* d, int offset);
 
 /*  */
 int compact_dazibao(struct dazibao* d);
+#endif /* DAZIBAO_H */
