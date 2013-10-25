caa398c48450febd6ea1b0f05981bf685a74a572
Baptiste Fontaine <batifon@yahoo.fr>
2013-10-25 12:15:52 +0200

    compact_dazibao implemented (untested)

--- src/dazibao.c
+++ src/dazibao.c
@@ -8,7 +8,7 @@ int close_dazibao(struct dazibao* d) {
 	return 0;
 }
 
-int read_tlv(struct dazibao* d, struct tlv* buf, int offset) {
+int read_tlv(struct dazibao* d, struct tlv* buf, offset_t offset) {
 	return 0;
 }
 
@@ -16,7 +16,7 @@ int next_tlv(struct dazibao* d, struct tlv* buf) {
 	return 0;
 }
 
-int tlv_at(struct dazibao* d, struct tlv*, int offset) {
+int tlv_at(struct dazibao* d, struct tlv*, offset_t offset) {
 	return 0;
 }
 
@@ -24,10 +24,76 @@ int add_tlv(struct dazibao* d, struct tlv*) {
 	return 0;
 }
 
-int rm_tlv(struct dazibao* d, int offset) {
+int rm_tlv(struct dazibao* d, offset_t offset) {
 	return 0;
 }
 
 int compact_dazibao(struct dazibao* d) {
-	return 0;
+
+#define BUFFLEN 128
+
+        struct tlv tlv_buf;
+        off_t reading  = DAZIBAO_HEADER_SIZE,
+              writing  = DAZIBAO_HEADER_SIZE;
+
+        int saved   = 0,
+            readlen,
+            len;
+
+        char buff[BUFFLEN];
+
+        if (d == NULL) {
+                return saved;
+        } 
+
+
+        if (lseek(d->fd, reading, SEEK_SET) < 0) {
+                return -1;
+        }
+
+        while(tlv_at(d, &tlv_buf, reading) > 0) {
+
+                len = SIZEOF_TLV(tlv_buf);
+
+                if (tlv_buf.type == PAD1 || tlv_buf.type == PADN) {
+                        reading += len;
+                        continue;
+                }
+
+                if (reading == writing) {
+                        reading += len;
+                        writing += len;
+                        continue;
+                }
+
+                saved += len;
+                while (len > 0) {
+
+                        if (lseek(d->fd, reading, SEEK_SET) < 0) {
+                                return -1;
+                        }
+
+                        readlen = read(d->fd, buff, MIN(len, BUFFLEN));
+                        if (readlen < 0) {
+                                return -1;
+                        }
+
+                        if (lseek(d->fd, writing, SEEK_SET) < 0) {
+                                return -1;
+                        }
+                        if (write(d->fd, buff, readlen) < 0) {
+                                return -1;
+                        }
+
+                        reading += readlen;
+                        writing += readlen;
+                        len     -= readlen;
+                }
+        }
+
+        ftruncate(d->fd, reading);
+
+	return saved;
+
+#undef BUFFLEN
 }
--- src/dazibao.h
+++ src/dazibao.h
@@ -10,6 +10,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "tlvs.h"
+#include "utils.h"
 
 
 #define PANIC(str) {					\
@@ -41,23 +42,24 @@ int open_dazibao(struct dazibao* d, char* path, int flags);
 int close_dazibao(struct dazibao* d);
 
 /*  */
-int read_tlv(struct dazibao* d, struct tlv* buf, int offset);
+int read_tlv(struct dazibao* d, struct tlv* buf, off_t offset);
 
 /*  */
 int next_tlv(struct dazibao* d, struct tlv* buf);
 
 /*  */
-int tlv_at(struct dazibao* d, struct tlv* buf, int offset);
+int tlv_at(struct dazibao* d, struct tlv* buf, off_t offset);
 
 /*  */
 int add_tlv(struct dazibao* d, struct tlv* buf);
 
 /*  */
-int rm_tlv(struct dazibao* d, int offset);
+int rm_tlv(struct dazibao* d, off_t offset);
 
 /*
  * Compact a Dazibao file. The file must have been opened in read/write mode,
- * and the Dazibao is NOT closed by the function.
+ * and the Dazibao is NOT closed by the function. Also, the dazibao offset is
+ * NOT preserved.
  * The function returns the number of bytes saved by the compacting operation,
  * or -1 if an error occured.
  */
