8dc0b14e685a1c9b39128a4b61c67e78c341ccc7
david <galichet.david@gmail.com>
2013-10-28 18:05:09 +0100

    fix to read call field length in next_tlv

--- src/dazibao.c
+++ src/dazibao.c
@@ -6,27 +6,27 @@ int open_dazibao(struct dazibao* d, const char* path, const int flags) {
 	char header[DAZIBAO_HEADER_SIZE];
 
 	fd = open(path, flags);
-	if(fd == -1) { /* open failed */
+	if (fd == -1) { /* open failed */
 		ERROR("open", -1);
 	}
 
-	if(flags & O_WRONLY) {
+	if (flags & O_WRONLY) {
 		lock = LOCK_EX;
-	} else if(flags & O_RDONLY) {
+	} else if (flags & O_RDONLY) {
 		lock = LOCK_SH;
 	} else {
 		CLOSE_AND_ERROR(fd, "bad flags", -1);
 	}
 	
-	if(flock(fd, lock) == -1) {
+	if (flock(fd, lock) == -1) {
 		CLOSE_AND_ERROR(fd, "flock", -1);
 	}
 	
-	if(read(fd, header, DAZIBAO_HEADER_SIZE) < DAZIBAO_HEADER_SIZE) {
+	if (read(fd, header, DAZIBAO_HEADER_SIZE) < DAZIBAO_HEADER_SIZE) {
 		CLOSE_AND_ERROR(fd, "not a dazibao", -1);
 	}
 	
-	if(header[0] != MAGIC_NUMBER || header[1] != 0) {
+	if (header[0] != MAGIC_NUMBER || header[1] != 0) {
 		/* calling perror makes no sens here... */
 		CLOSE_AND_ERROR(fd, "not a dazibao", -1);
 	}
@@ -38,11 +38,11 @@ int open_dazibao(struct dazibao* d, const char* path, const int flags) {
 
 int close_dazibao(struct dazibao* d) {
 
-	if(flock(d->fd, LOCK_UN) == -1) {
+	if (flock(d->fd, LOCK_UN) == -1) {
 		PANIC("flock:");
 		/* should it return an error intead ? */
 	}
-	if(close(d->fd) == -1) {
+	if (close(d->fd) == -1) {
 		PANIC("close:");
 		/* should it return an error intead ? */
 	}
@@ -55,16 +55,16 @@ int read_tlv(struct dazibao* d, struct tlv* buf, const off_t offset) {
 	/* probably some issues to fix with large tlv */
 
 	/* problem if buf->value was not init with malloc */
-	if(!realloc(buf->value, sizeof(*(buf->value)) * buf->length)) {
+	if (!realloc(buf->value, sizeof(*(buf->value)) * buf->length)) {
 		ERROR("malloc", -1);
 	}
 
-	if(lseek(d->fd, offset, SEEK_SET) == -1) {
+	if (lseek(d->fd, offset, SEEK_SET) == -1) {
 		ERROR("lseek", -1);
 	}
 
 
-	if(read(d->fd, buf->value, buf->length) < buf->length) {
+	if (read(d->fd, buf->value, buf->length) < buf->length) {
 		ERROR("read", -1);
 	}
 
@@ -75,25 +75,29 @@ off_t next_tlv(struct dazibao* d, struct tlv* buf) {
 
 	int size_read;
 	char tlv_type;
+        char tlv_length[SIZEOF_TLV_LENGTH];
 	off_t current;
 
-	if((current = lseek(d->fd, 0, SEEK_CUR)) == -1) {
+	if ((current = lseek(d->fd, 0, SEEK_CUR)) == -1) {
 		ERROR("lseek", -1);
 	}
 
-	if((size_read = read(d->fd, &tlv_type, SIZEOF_TLV_TYPE)) < 0) {
+	if ((size_read = read(d->fd, &tlv_type, SIZEOF_TLV_TYPE)) < 0) {
 		ERROR("next_tlv read type", -1);
-	} else if(!size_read) {
+	} else if (!size_read) {
 		return EOD;
 	}
 
 	buf->type = tlv_type; 
 	
-	if(tlv_type != TLV_PAD1) {
-	        if(read(d->fd, &(buf->length), SIZEOF_TLV_LENGTH ) < SIZEOF_TLV_LENGTH) {
+	if (tlv_type != TLV_PAD1) {
+	        if (read(d->fd, &tlv_length, SIZEOF_TLV_LENGTH ) 
+                                < SIZEOF_TLV_LENGTH) {
 		        ERROR("next_tlv read length", -1);
 	        }		
-                if(lseek(d->fd, buf->length, SEEK_CUR) == -1) {
+                buf->length = (tlv_length[0] << 16) + (tlv_length[1] << 8) +
+                              tlv_length[2]; 
+                if (lseek(d->fd, buf->length, SEEK_CUR) == -1) {
                         ERROR("next_tlv lseek next_tlv", -1);
                 }
         }
@@ -105,24 +109,24 @@ int tlv_at(struct dazibao* d, struct tlv* buf, const off_t offset) {
 
 	off_t current;
 	
-	if((current = lseek(d->fd, 0, SEEK_CUR)) == -1) {
+	if ((current = lseek(d->fd, 0, SEEK_CUR)) == -1) {
 		ERROR("lseek", -1);
 	}
 	
-	if(lseek(d->fd, offset, SEEK_SET) == -1) {
+	if (lseek(d->fd, offset, SEEK_SET) == -1) {
 		ERROR("lseek", -1);
 	}
 
-	if(next_tlv(d, buf) <= 0) {
+	if (next_tlv(d, buf) <= 0) {
 
-		if(lseek(d->fd, current, SEEK_SET) == -1) {
+		if (lseek(d->fd, current, SEEK_SET) == -1) {
 			ERROR("lseek", -1);
 		}
 	
 		return -1;
 	}
 
-	if(lseek(d->fd, current, SEEK_SET) == -1) {
+	if (lseek(d->fd, current, SEEK_SET) == -1) {
 		ERROR("lseek", -1);
 	}
 
