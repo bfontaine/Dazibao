7f9c80e557185062649bccc1f16c46efa01adac0
Baptiste Fontaine <batifon@yahoo.fr>
2013-11-15 00:00:01 +0100

    copyediting + parse_request fixed

--- src/web/request.h
+++ src/web/request.h
@@ -17,11 +17,11 @@ char is_crlf(char *s, int c, int len);
 /**
  * This function is a wrapper around recv to read the beginning of the input on
  * a socket line-by-line. It takes the file descriptor of a socket and return a
- * NULL-terminated string representing the next line in the input. On the end of
- * the input, the rest of the buffer is returned and eoh is set to its length.
- * If an error occured, the function returns NULL and sets errno. It's used to
- * read the headers of an HTTP request.  The returned string is dynamically
- * allocated, so you'll need to free it later.
+ * NULL-terminated string representing the next line in the input. On the end
+ * of the input, the rest of the buffer is returned and eoh is set to its
+ * length.  If an error occured, the function returns NULL and sets errno. It's
+ * used to read the headers of an HTTP request.  The returned string is
+ * dynamically allocated, so you'll need to free it later.
  **/
 char *next_header(int sock, int *eoh);
 
