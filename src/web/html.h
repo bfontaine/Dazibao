dc9126571d4f5f696e217ec5bc862516b76ec80a
Baptiste Fontaine <batifon@yahoo.fr>
2013-11-20 21:00:36 +0100

    tlv2html signature fixed (taking a TLV instead of trying to create one from scratch with read_tlv

--- src/web/html.h
+++ src/web/html.h
@@ -2,11 +2,12 @@
 #define _HTML_H 1
 
 #include "dazibao.h"
+#include "tlv.h"
 
 /**
  * Make an HTML representation of a TLV, NULL-terminated.
  **/
-int tlv2html(dz_t dz, off_t off, char **html);
+int tlv2html(dz_t dz, tlv_t t, off_t off, char **html);
 
 /**
  * Make an HTML representation of a dazibao, NULL-terminated.
