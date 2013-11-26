5663c1cd0818de68f2f7e3228e73ff70ac907515
Baptiste Fontaine <batifon@yahoo.fr>
2013-11-26 10:36:59 +0100

    HTML representation of texts + beginning of images

--- src/web/html.h
+++ src/web/html.h
@@ -16,7 +16,13 @@
                       "</head>\n" \
                       "<body>\n" \
                         "<ol class=\"tlvs\">\n"
-
+#define HTML_TLV_FMT(F)   "<li class=\"tlv\">\n" \
+                           "<div class=\"metadata\">" \
+                             "<span class=\"type\">%d</span>" \
+                             " <span class=\"length\">%u</span>" \
+                           "</div>\n" \
+                           "<div class=\"value\">" F "</div>\n" \
+                         "</li>"
 #define HTML_DZ_BOTTOM  "</ol>\n" \
                       "</body>\n" \
                     "</html>"
@@ -25,6 +31,26 @@
 #define HTML_DZ_TOP_LEN strlen(HTML_DZ_TOP)
 #define HTML_DZ_BOTTOM_LEN strlen(HTML_DZ_BOTTOM)
 
+/* Use HTML_TLV_FMT(F) to declare the format of a TLV HTML, e.g.:
+ *      char fmt[] = HTML_TLV_FMT("<p>%s</p>");
+ */
+
+#define JPEG_EXT ".jpg"
+#define PNG_EXT  ".png"
+#define DEFAULT_EXT ""
+
+/**
+ * Make a NULL-terminated HTML representation of a TLV, assuming its value is
+ * in plain text.
+ **/
+int text_tlv2html(tlv_t *t, int type, unsigned int len, char *html);
+
+/**
+ * Make a NULL-terminated HTML representation of an image TLV (png or jpeg).
+ * 'ext' should be either JPEG_EXT or PNG_EXT.
+ **/
+int img_tlv2html(tlv_t *t, int type, unsigned int len, off_t off,
+                char *html, const char *ext);
 
 /**
  * Make a NULL-terminated HTML representation of a TLV.
