6ab934ffba035716904e3061ffa0297679ccb511
Baptiste Fontaine <batifon@yahoo.fr>
2013-12-03 01:14:41 +0100

    some WLOGDEBUG calls added

--- src/web/mime.c
+++ src/web/mime.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <string.h>
+#include "webutils.h"
 #include "mime.h"
 
 const char *get_mime_type(const char *path) {
@@ -6,13 +8,19 @@ const char *get_mime_type(const char *path) {
         if (path == NULL) {
                 return NULL;
         }
+
+        WLOGDEBUG("Looking for a mime type for '%s'", path);
+
         if ((dot = strrchr(path, '.')) == NULL) {
+                WLOGDEBUG("No dot = no extension = no mime type");
                 return NULL;
         }
         dot++;
+        WLOGDEBUG("extension: %s", dot);
 
         for (unsigned int i=0; i<MIME_TYPES_COUNT; i++) {
                 if (strcasecmp(mime_types_ext[i][0], dot) == 0) {
+                        WLOGDEBUG("Using '%s'", mime_types_ext[i][1]);
                         return mime_types_ext[i][1];
                 }
         }
