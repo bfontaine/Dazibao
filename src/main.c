41aa5db4b6dfc0e8acfd8a4a2267e29efe410e94
david <galichet.david@gmail.com>
2013-12-04 12:59:10 +0100

    cutting diffrrent cmd to main and add function print usage

--- src/main.c
+++ src/main.c
@@ -4,8 +4,11 @@
 
 #define BUFFSIZE 512
 
-int cmd_add(int argc ,char **argv){
+int cmd_add(int argc, char **argv) {
         long tmp_type;
+	char *daz, *cmd;
+        cmd = argv[1];
+	daz = argv[0];
         if (argc < 4) {
                 printf("expected type\n");
                 exit(EXIT_FAILURE);
@@ -16,9 +19,8 @@ int cmd_add(int argc ,char **argv){
                 printf("unrecognized type\n");
                 exit(EXIT_FAILURE);
         }
-        type = (unsigned char)tmp_type;
 
-        if (add(daz, cmd, tmp_type) == -1) {
+        if (action_add(daz, cmd, tmp_type) == -1) {
                 printf("add error\n");
                 exit(EXIT_FAILURE);
         }
@@ -26,7 +28,7 @@ int cmd_add(int argc ,char **argv){
 }
 
 
-int add(char *daz, char *cmd, unsigned char type) {
+int action_add(char *daz, char *cmd, unsigned char type) {
 	dz_t daz_buf;
         char reader[BUFFSIZE],
              *buff = NULL;
@@ -70,10 +72,16 @@ int add(char *daz, char *cmd, unsigned char type) {
         }
 
         free(tlv);
-        free(buff);
+        if (buff != NULL) {
+                free(buff);
+        }
+        return 0;
 }
 
-int cmd_rm(int argc , int argv){
+int cmd_rm(int argc , char ** argv) {
+	dz_t daz_buf;
+	char *daz;
+	daz = argv[1];
 
         if (argc < 4) {
                 printf("expected offset\n");
@@ -105,7 +113,10 @@ int cmd_rm(int argc , int argv){
         return 0;
 }
 
-int cmd_dump(int argc , char ** argv){
+int cmd_dump(int argc , char ** argv) {
+	dz_t daz_buf;
+	char *daz;
+	daz = argv[1];
 
         if (dz_open(&daz_buf, daz, O_RDONLY)) {
                 exit(EXIT_FAILURE);
@@ -158,7 +169,10 @@ int cmd_dump(int argc , char ** argv){
 
 }
 
-int cmd_create(int argc , char ** argv){
+int cmd_create(int argc , char ** argv) {
+	dz_t daz_buf;
+	char *daz;
+	daz = argv[1];
 
         if (dz_create(&daz_buf, daz)) {
                 printf("error during dazibao creation\n");
@@ -172,7 +186,10 @@ int cmd_create(int argc , char ** argv){
         return 0;
 
 }
-int cmd_compact(int argc , char ** argv){
+int cmd_compact(int argc , char ** argv) {
+	dz_t daz_buf;
+	char *daz;
+	daz = argv[1];
         if (dz_open(&daz_buf, daz, O_RDWR)) {
                 exit(EXIT_FAILURE);
         }
@@ -189,32 +206,38 @@ int cmd_compact(int argc , char ** argv){
 
 }
 
-int helper(int msg){
-        /* switch msg
-                test si cmd non conforme out inconnue
-                si option valide
-                si argument valide dazibao ou fichier de tlv
-        */
-        return 0;
+void print_usage() {
+        printf("Usage:\n\t.dazibao <cmd> <option and args> <dazibao>\n");
+        printf("Cmd :\n");
+        printf("\tcreate  : create a empty dazibao or \
+        dazibao merges into arguments\n\tcmd : create [-m or --merge ] \
+        <dazibao args to merge> <dazibao>>\n\t option create:\n\
+        \t\t-m or --merge :(todo)\n");
+        printf("\t- add : add TLV \n\tcmd : add [-d or --date ]\
+        [-c or --compound ] <tlv args> <dazibao>\n\tadd option:\n\
+        \t\t\t-d or --date : (todo)\n\t\t\t-c or --compound :(todo)\n");
+        printf("\t- rm  : remove TLV\ncmd : rm <offset> <dazibao>\n");
+        printf("\t- dump : dump Dazibaio\ncmd : dump [-d or --debug ]\
+        [-D or --depth] <number depth> <dazibao>\n\tdump option :\n\
+        \t\t-D or --depth : (todo)\n\t\t-d or --debug :(todo)\n");
+        printf("\t- compact : compact dazibao\ncmd : compact [-r or\
+        --recusive] <dazibao>\n\tcompact option :\n\
+        \t\t-r or -- recursive :(todo)\n)");
 }
 
 int main(int argc, char **argv) {
-
-	dz_t daz_buf;
-	char *daz, *cmd;
+	char *cmd;
 
         if (setlocale(LC_ALL, "") == NULL) {
                 PERROR("setlocale");
         }
 
         if (argc < 3) {
-                printf("Usage:\n\t%s <dazibao> <cmd>\n", argv[0]);
+                print_usage();
                 exit(EXIT_FAILURE);
         }
 
-//	daz = argv[1];
 	cmd = argv[1];
-        // cree tableau des arguments
 
 	if ( !strcmp(cmd, "add")) {
                 /* cmd reconnue add
@@ -243,17 +266,15 @@ int main(int argc, char **argv) {
 	} else if (!strcmp(cmd, "create")) {
                 /* cmd create accepted
                         create <option & argument> <nom du dazibao>
-                option : 
-                --fusion -f 2 argument <dazibao> 
-
-
+                option :
+                --fusion -f 2 argument <dazibao>
                 */
 	} else if (!strcmp(cmd, "compact")) {
                 /* cmd compact
                 compact <option & argument> <dazibao>
                 */
 	} else {
-                //appeller helper avec cmd unknow
+                /*appeller helper avec cmd unknow*/
                 exit(EXIT_FAILURE);
         }
 
