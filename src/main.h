72e2aad01894f306ea6a92b254bbcddc6f101f69
david <galichet.david@gmail.com>
2013-12-04 14:52:31 +0100

    add args fic dazibao in all function cmd_<action>

--- src/main.h
+++ src/main.h
@@ -2,10 +2,10 @@
 #define _MAIN_H 1
 #endif
 
-int cmd_add(int argc, char ** argv);
+int cmd_add(int argc, char ** argv, char * daz);
 int action_add(char *daz, char *cmd, unsigned char type);
-int cmd_rm(int argc, char ** argv);
-int cmd_dump(int argc, char ** argv);
-int cmd_compact(int argc, char ** argv);
-int cmd_create(int argc, char ** argv);
+int cmd_rm(int argc, char ** argv, char * daz);
+int cmd_dump(int argc, char ** argv, char * daz);
+int cmd_compact(int argc, char ** argv, char * daz);
+int cmd_create(int argc, char ** argv, char * daz);
 void print_usage();
