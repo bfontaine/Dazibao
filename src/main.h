53d2d911ec510e30ae1d40ddfa7a5706aa119cc6
david <galichet.david@gmail.com>
2013-12-04 12:56:45 +0100

    create function to different cmd or managment this cmd to dazibao

--- src/main.h
+++ src/main.h
@@ -1,3 +1,11 @@
 #ifndef _MAIN_H
 #define _MAIN_H 1
 #endif
+
+int cmd_add(int argc, char ** argv);
+int action_add(char *daz, char *cmd, unsigned char type);
+int cmd_rm(int argc, char ** argv);
+int cmd_dump(int argc, char ** argv);
+int cmd_compact(int argc, char ** argv);
+int cmd_create(int argc, char ** argv);
+void print_usage();
