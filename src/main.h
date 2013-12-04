86cbc6fa75ae08416821f90c4f2eb36f33df6a1f
david <galichet.david@gmail.com>
2013-12-04 20:31:19 +0100

    function manage flag to option dump

--- src/main.h
+++ src/main.h
@@ -6,7 +6,7 @@ int cmd_add(int argc, char ** argv, char * daz);
 int action_add(char *daz, unsigned char type);
 int cmd_rm(int argc, char ** argv, char * daz);
 int cmd_dump(int argc, char ** argv, char * daz);
-int action_dump(int argc, char ** argv, char * daz);
+int action_dump(char *daz, int flag_debug, int flag_depth);
 int cmd_compact(int argc, char ** argv, char * daz);
 int cmd_create(int argc, char ** argv, char * daz);
 void print_usage();
