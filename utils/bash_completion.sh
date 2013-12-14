52d1c1e15763094999044885f9a6bce961e369c3
Baptiste Fontaine <batifon@yahoo.fr>
2013-12-14 19:47:39 +0100

    Bash autocomplete for dazibao & daziweb

--- utils/bash_completion.sh
+++ utils/bash_completion.sh
@@ -2,24 +2,55 @@
 # Source it somewhere in your .bashrc,
 # or copy it in /etc/bash_completion.d/
 
+DZ_EXTS=${DZ_EXTS:-dz|dzb|dbz|daz|dazibao|}
+
 _daziweb() {
 
-    local cur
+    local cur prev
 
+    COMPREPLY=()
     cur=${COMP_WORDS[COMP_CWORD]}
+    prev=${COMP_WORDS[COMP_CWORD-1]}
 
-    case "$cur" in
-        -*)
-        COMPREPLY=( $( compgen -W '-v -l -p -d' -- $cur )) ;;
+    OPTS='-d -D -h -p -v'
 
+    case "$prev" in
+        -d)
+            COMPREPLY=( $( compgen -fX "!*.@($DZ_EXTS)" -o plusdirs -- $cur ) ) ;;
         *)
-        COMPREPLY=( $( compgen -fX '!*.@(dzb|dazibao)' -o plusdirs -- $cur ) ) ;;
+            COMPREPLY=( $( compgen -W "$OPTS" -- $cur )) ;;
     esac
 
     return 0
 
 }
 
-# TODO dazibao
+_dazibao() {
+
+    local cmd cur prev
+
+    COMPREPLY=()
+    cmd=${COMP_WORDS[1]}
+    cur=${COMP_WORDS[COMP_CWORD]}
+    prev=${COMP_WORDS[COMP_CWORD-1]}
+
+    COMMANDS='add compact create dump rm'
+
+    case "$cmd" in
+        # TODO add subcommands-specific options here
+        add)     completions='' ;;
+        compact) completions='' ;;
+        create)  completions='' ;;
+        dump)    completions='' ;;
+        rm)      completions='' ;;
+        *)
+            COMPREPLY=( $( compgen -W "$COMMANDS" -- $cur ))
+            return 0 ;;
+    esac
+
+    COMPREPLY=( $( compgen -W "$completions" -fX "!*.@($DZ_EXTS)" -o plusdirs -- $cur ));
+    return 0
+}
 
 complete -F _daziweb -o filenames daziweb
+complete -F _dazibao -o filenames dazibao
