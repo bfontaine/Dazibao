7a3ceae69c71180affd4434fa94e7aae789818f3
Baptiste Fontaine <batifon@yahoo.fr>
2013-12-16 23:07:51 +0100

    strings comparison fixed

--- tests/tests.h
+++ tests/tests.h
@@ -18,31 +18,48 @@
 extern int __tests_ct;
 extern int __tests_success;
 
+/* call this at the beginning of your tests */
 #define START_TESTS() __tests_ct=0,__tests_success=0
+
+/* call this at the top of your tests file */
 #define INIT_TESTS() int __tests_ct=0,__tests_success=0
 
-/* don't use this macro directly */
-#define MYTEST(_t,fmt,expr,expected) \
-        { _t res = (expr); __tests_ct++; \
-          if (res != (_t)(expected)) { \
-                  printf(ANSI_COLOR_RED "[FAIL] '" S(expr) "' -> '" \
-                                  fmt "', expected '" fmt "'" EOTESTLINE, \
-                                  res, (_t)expected); \
-          } else { __tests_success++; \
-                  printf(ANSI_COLOR_GREEN "[ OK ] '" S(expr) "' -> '" \
-                                  fmt "'" EOTESTLINE, res); } }
+/** used as a comparison function for numbers. t=type */
+#define N_CMP(a,b) ((a) == (b))
+/** used as a comparison function for strings. t=char* */
+#define S_CMP(a,b) (((a)==NULL && (b)==NULL) \
+                        || ((a)!=NULL && (b)!=NULL && strcmp((a), (b)) == 0))
 
+/* don't use this macro directly
+ *
+ * this stores 'expr' in 'res', and 'expected' in '_exp', where both are
+ * variable of type '_t', and 'res' is constant.  It then increments
+ * '__tests_ct' and compares 'res' and '_exp' using 'cmp', which should be a
+ * 2-parameters comparison macro/function. If the result is truthy, it then
+ * increments '__tests_success' and print a message. If the result is falsy
+ * (0), it prints an error message.
+ * */
+#define MYTEST(_t,cmp,fmt,expr,expected) \
+        { const _t res = (expr);_t _exp=expected; __tests_ct++; \
+          if (cmp(res, (_t)(_exp))) { __tests_success++; \
+                  printf(ANSI_COLOR_GREEN "[ OK ] '" S(expr) "' -> '" \
+                                  fmt "'" EOTESTLINE, res); \
+          } else { printf(ANSI_COLOR_RED "[FAIL] '" S(expr) "' -> '" \
+                                  fmt "', expected '" fmt "'" EOTESTLINE, \
+                                  res, (_t)_exp); } }
+
+/* call this at the end of the tests */
 #define END_OF_TESTS() \
         { int p=((int)((100*__tests_success/(__tests_ct+0.0))*10)/10); \
           printf(ANSI_COLOR_CYAN "Total: %d%% (%d/%d)" EOTESTLINE, \
                           p, __tests_success, __tests_ct); }
 
 /** 'int' test. x = expression, e = expected */
-#define INTTEST(x,e)  MYTEST(int,  "%d",  x,e)
+#define INTTEST(x,e)  MYTEST(int, N_CMP, "%d",  x, e)
 /** 'long' test. x = expression, e = expected */
-#define LONGTEST(x,e) MYTEST(long, "%li", x,e)
+#define LONGTEST(x,e) MYTEST(long, N_CMP, "%li", x, e)
 /** 'char*' test. x = expression, e = expected */
-#define STRTEST(x,e)  MYTEST(char*, "%s", x,e)
+#define STRTEST(x,e)  MYTEST(char*, S_CMP, "%s", x, e)
 
 /* Add a test suite */
 #define ADD_SUITE(N) \
