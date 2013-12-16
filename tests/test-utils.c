37751ee4dde551ee522db50f704038ac0fa0750b
Baptiste Fontaine <batifon@yahoo.fr>
2013-12-16 23:08:03 +0100

    get_ext tests added

--- tests/test-utils.c
+++ tests/test-utils.c
@@ -26,11 +26,20 @@ void str2dec_positive_tests(void) {
         LONGTEST(str2dec_positive(""), -1);
         LONGTEST(str2dec_positive(NULL), -1);
 }
+void get_ext_tests(void) {
+        STRTEST(get_ext("foo.bar"), "bar");
+        STRTEST(get_ext("foo.bar.baz"), "baz");
+        STRTEST(get_ext("foo.42"), "42");
+        STRTEST(get_ext("foo."), "");
+        STRTEST(get_ext("foobar"), NULL);
+        STRTEST(get_ext(NULL), NULL);
+}
 
 int main(void) {
         START_TESTS();
 
         ADD_SUITE(str2dec_positive);
+        ADD_SUITE(get_ext);
 
         END_OF_TESTS();
         return 0;
