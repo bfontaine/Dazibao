9bdea40bf240a2eeed4d635ad38e7c29fa37d3ea
Julien Sagot <ju.sagot@gmail.com>
2013-12-23 16:00:50 +0100

    Fixed missing declaration

--- src/cli/cli.h
+++ src/cli/cli.h
@@ -9,10 +9,12 @@
 #define CLI_USAGE_FMT "RTFM"
 
 #include "tlv.h"
+#include "mdazibao.h"
 
 int cli_mk_tlv(tlv_t *tlv, int argc, char **argv, char *type, char date);
 int cli_add(int argc, char **argv);
 int cli_dump_tlv(int argc, char **argv, int out);
+int cli_print_dz(dz_t *dz, int indent, int lvl, int debug);
 int cli_dump_dz(int argc, char **argv, int out);
 int cli_compact_dz(char *file);
 #endif
